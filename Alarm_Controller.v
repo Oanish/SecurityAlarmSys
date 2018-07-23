a6953b896d67d746a57518dc3c7bc243d9451b46
Oanish <niculescu.oana@yahoo.com>
2018-07-23 22:21:31 +0300

    Removed Audio Module (Noise Error)

--- Alarm_Controller.v
+++ Alarm_Controller.v
@@ -1,7 +1,15 @@
 module Alarm_Controller(input CLK,					//trigger clock, used in all modules and by the DAC decoder
+								input RST,					//system reset
 								input [7:0] Distance,	//distance from object, bits
-								output Sound);				//alarm data stream to be converted to analog audio
+								output reg Sound);		//alarm data stream to be converted to analog audio
 								
-//do stuff here
+always@(posedge CLK)
+	if(!RST)
+		Sound <= 0;
+	else
+		if(Distance < 100)
+			Sound <= 1;
+		else
+			Sound <= 0;
 
 endmodule 
--- Security_Top.v
+++ Security_Top.v
@@ -1,12 +1,12 @@
 module Security_Top (input CLK,						//the board's oscillator clock
 							input RST,						//system reset
 							input Echo_Sig,				//echo signal from HC-SR04
-							output Trigger_Sig,			//trigger signal to HC-SR04
+							output Trigger_Sig);			//trigger signal to HC-SR04
 							//output [13:0] Distance,	//distance from object, digits
-							output T_CLK,					//trigger clock, used in all modules and by the DAC decoder
-							output Sound_Sig);			//alarm data stream to be converted to analog audio
+							//output T_CLK,				//trigger clock, used in all modules and by the DAC decoder
+							//output Sound_Sig);			//alarm data stream to be converted to analog audio
 							
-//wire T_CLK;						//trigger clock, used in all modules
+wire T_CLK;						//trigger clock, used in all modules
 wire [7:0] Distance_Raw;	//distance from object, bits
 							
 CLK_Divider DUT1(.CLK(CLK),
@@ -22,8 +22,11 @@ Sensor_Controller DUT2(.CLK(T_CLK),
 //Digit_Converter DUT3(.Distance_Raw(Distance_Raw),
 //							.Distance(Distance));
 
-Alarm_Controller DUT4(.CLK(T_CLK),
-							 .Distance(Distance_Raw),
-							 .Sound(Sound_Sig));
+//Alarm_Controller DUT4(.CLK(T_CLK),					//Not working. Sending noise
+//							 .RST(RST),
+//							 .Distance(Distance_Raw),
+//							 .Sound(Sound_Sig));
+							
+//IoT module
 							
 endmodule 
