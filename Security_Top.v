4b24135032cfbbc792553de6e08b0a134d4ae49a
Oanish <niculescu.oana@yahoo.com>
2018-07-23 21:00:51 +0300

    Removed Distance Calc Introduced Alarm Skeleton

--- Security_Top.v
+++ Security_Top.v
@@ -2,11 +2,12 @@ module Security_Top (input CLK,						//the board's oscillator clock
 							input RST,						//system reset
 							input Echo_Sig,				//echo signal from HC-SR04
 							output Trigger_Sig,			//trigger signal to HC-SR04
-							output [13:0] Distance,		//distance from object, digits
-							output [7:0] Distance_Raw);//distance from object, bits
+							//output [13:0] Distance,	//distance from object, digits
+							output T_CLK,					//trigger clock, used in all modules and by the DAC decoder
+							output Sound_Sig);			//alarm data stream to be converted to analog audio
 							
-wire T_CLK;						//trigger clock, used in all modules
-//wire [7:0] Distance_Raw;	//distance from object, bits
+//wire T_CLK;						//trigger clock, used in all modules
+wire [7:0] Distance_Raw;	//distance from object, bits
 							
 CLK_Divider DUT1(.CLK(CLK),
 					  .RST(RST),
@@ -18,7 +19,11 @@ Sensor_Controller DUT2(.CLK(T_CLK),
 							  .Echo(Echo_Sig),
 							  .Distance(Distance_Raw));
 						 
-Digit_Converter DUT3(.Distance_Raw(Distance_Raw),
-							.Distance(Distance));
+//Digit_Converter DUT3(.Distance_Raw(Distance_Raw),
+//							.Distance(Distance));
+
+Alarm_Controller DUT4(.CLK(T_CLK),
+							 .Distance(Distance_Raw),
+							 .Sound(Sound_Sig));
 							
 endmodule 
